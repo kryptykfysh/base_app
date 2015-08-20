b7694b3a2b2c640a6e9bff7d28289094b8517b40
Kryptykfysh <kryptykfysh@kryptykfysh.co.uk>
2015-08-20 22:16:13 +1000

    Configure DatabaseCleaner.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,4 +57,25 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # DatabaseCleaner configuration
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each)  do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
